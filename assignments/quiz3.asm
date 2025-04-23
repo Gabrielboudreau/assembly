7f7166f4122f8439acf4eb65f854f1b5d74031b8
Gabrielboudreau <89223461+Gabrielboudreau@users.noreply.github.com>
2025-04-23 11:07:08 -0800

    Update quiz3.asm

--- assignments/quiz3.asm
+++ assignments/quiz3.asm
@@ -1,1 +1,29 @@
+section .text
+        global _start
 
+_start:
+
+        mov eax, [var] ;this will be used to store the cumulative value
+        mov ebx, [var] ;this will act as a counter
+        mov ecx, 0 ; this will be compared to our counter so we know when to stop iteration
+
+
+comp:
+        imul eax, ebx ;multiply for factorial
+        dec ebx ;iterate
+        cmp ebx, ecx
+        jg comp
+
+
+done:
+        mov [result], eax
+        mov eax, 1 ;exit
+    mov ebx, 0
+        int 0x80
+
+
+section .bss
+        result resd 1
+
+section .data
+        var DD 5 ;any number can be used, using 5 for example
