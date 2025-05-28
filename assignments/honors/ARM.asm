a5e74cfe2c5c46e155ab70efcc30a680369e0987
Gabrielboudreau <89223461+Gabrielboudreau@users.noreply.github.com>
2025-05-27 21:30:13 -0700

    Create ARM.asm

---
+++ assignments/honors/ARM.asm
@@ -0,0 +1,30 @@
+.section .text
+	.global _start
+
+_start: 
+	LDR r1 =var1
+	LDR r2, [r1]
+	LDR r1, =var2
+	LDR r3, [r1]
+	ADD r4, r2,r3
+	LDR r5, [r1]
+	MUL r0,r4,r5
+	LDR r1 =var4
+	STR r0,r1
+	MOV r7,#1
+	MOV r0, #0
+	SVC #0
+
+	.section .data
+var1: .word 5
+var2: .word 2
+var3: .word 3
+
+	.section .bss
+	.align 4
+var4 .space 4
+@This requires several more instructions to exit
+@Ensuring that you load each value through is time consuming and repetitive
+@Each instrcution in ARM is relatively simple making it easy to walk through the code
+@ARM is faster and more effiecient with power meaning for extremely limited quantities
+@or precise coding it could be more effective
